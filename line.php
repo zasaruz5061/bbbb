80fe7a7b7fccc41aef3d4c439e2cf322856036e1
zasaruz5061 <48837868+zasaruz5061@users.noreply.github.com>
2019-03-23 02:09:20 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'QfJBJ6tYD2EmqepyV2FxSD10lP5BCd4oz5xuRX/0xPFLlsMcLKt1d8/IjhDi+Zpwp8SgQtIwQWvlUv1IhIUvxyE8vUSOSvg8aVh9mOElmG6KMWBZP1FZY2eQzmoGXY78OYe9nNP4O8y+d/4+R+cNkwdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',

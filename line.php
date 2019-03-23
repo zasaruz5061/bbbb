21c8e05520a56f50ceaa7eb4de485e5cc4362bff
zasaruz5061 <48837868+zasaruz5061@users.noreply.github.com>
2019-03-23 11:02:59 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ufbc178fddebad6d21b4f80a9ea8b6192',
+        'to' => '$event->getReplyToken()',
         'messages' => [$messages],
       ];
       $post = json_encode($data);

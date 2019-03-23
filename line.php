026b8a9d9fc8b9f8cc1423305f7c67ed5426975d
zasaruz5061 <48837868+zasaruz5061@users.noreply.github.com>
2019-03-23 11:44:17 +0700

    Update line.php

--- line.php
+++ line.php
@@ -13,7 +13,7 @@ function send_LINE($msg){
       // Make a POST Request to Messaging API to reply to sender
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
-        'to' => $reply,
+        'to' => 'U...',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
